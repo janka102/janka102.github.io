a5c3558dd4e07e98c791d5f5d8ba9ced2b08ebf7
Jesse Smick <janka102@gmail.com>
2015-02-24 05:48:35 -0800

    Trying out react-router

---
+++ js/routes.js
@@ -0,0 +1,124 @@
+var Router = ReactRouter;
+
+var DefaultRoute = Router.DefaultRoute;
+var Link = Router.Link;
+var Route = Router.Route;
+var RouteHandler = Router.RouteHandler;
+
+var App = React.createClass({displayName: "App",
+    render:function() {
+        return (
+            React.createElement("div", {className: "row"}, 
+                React.createElement("div", {className: "col-xs-12"}, 
+                    React.createElement("nav", null, 
+                        React.createElement("ul", {className: "tabs"}, 
+                            React.createElement("li", {className: "tab col-xs-6"}, React.createElement(Link, {to: "app"}, "Home")), 
+                            React.createElement("li", {className: "tab col-xs-6"}, React.createElement(Link, {to: "contact"}, "Contact"))
+                        )
+                    )
+                ), 
+                React.createElement(RouteHandler, null)
+            )
+        );
+    }
+});
+
+var About = React.createClass({displayName: "About",
+    render:function() {
+        return (
+            React.createElement("div", {className: "col-xs-10 col-xs-offset-1 body"}, 
+                React.createElement("h4", {className: "text-center"}, "Hi, my name is ", React.createElement("b", null, "Jesse Smick"), ", and I'm a student and part-time developer."), 
+                React.createElement("p", null, "I mainly develop for the web, both client-side and server side.")
+            )
+        );
+    }
+});
+
+var contactMethods = [{
+    title: 'Email',
+    href: 'mailto:jesse@smick.me',
+    icon: 'envelope-o',
+    color: 'red lighten-1'
+}, {
+    title: 'Bitcoin',
+    href: '/btc',
+    icon: 'btc',
+    color: 'amber darken-1'
+}, {
+    title: 'GitHub',
+    href: '/gh',
+    icon: 'github',
+    color: 'blue lighten-1'
+}, {
+    title: 'Google+',
+    href: '/g+',
+    icon: 'google-plus',
+    color: 'red'
+}, {
+    title: 'Hacker News',
+    href: '/hn',
+    icon: 'hacker-news',
+    color: 'orange accent-4'
+}, {
+    title: 'Reddit',
+    href: '/reddit',
+    icon: 'reddit',
+    color: 'light-blue darken-2'
+}, {
+    title: 'Stack Overflow',
+    href: '/so',
+    icon: 'stack-overflow',
+    color: 'deep-orange'
+}, {
+    title: 'Twitter',
+    href: '/twitter',
+    icon: 'twitter',
+    color: 'light-blue lighten-1'
+}, {
+    title: 'Keybase',
+    href: '/keybase',
+    icon: 'key',
+    color: 'yellow darken-1'
+}, {
+    title: 'Bitbucket',
+    href: '/bitbucket',
+    icon: 'bitbucket',
+    color: 'blue darken-4'
+}];
+
+var Contact = React.createClass({displayName: "Contact",
+    render:function() {
+        var contactTiles = [];
+
+        for (var i = 0, contactMethod; i < contactMethods.length; i++) {
+            contactMethod = contactMethods[i];
+            contactTiles.push(
+                React.createElement("div", {className: "col-xs-6 col-sm-4 col-md-3 tile", key: contactMethod.title}, 
+                  React.createElement("a", {href: contactMethod.href, title: contactMethod.title, className: 'tile-content ' + contactMethod.color}, 
+                    React.createElement("div", {className: "valign-wrapper tile-icon"}, 
+                      React.createElement("i", {className: 'fa fa-' + contactMethod.icon + ' valign'})
+                    ), 
+                    React.createElement("div", {className: "tile-data"}, 
+                      React.createElement("div", {className: "tile-title"}, contactMethod.title)
+                    )
+                  )
+                )
+            );
+        }
+
+        return (
+                React.createElement("div", {className: "row body grid"}, contactTiles)
+        );
+    }
+});
+
+var routes = (
+    React.createElement(Route, {name: "app", path: "/", handler: App}, 
+        React.createElement(Route, {name: "contact", handler: Contact}), 
+        React.createElement(DefaultRoute, {handler: About})
+    )
+);
+
+Router.run(routes, function(Handler) {
+    React.render(React.createElement(Handler, null), document.getElementsByTagName('main')[0]);
+});
---
+++ js/routes.jsx
@@ -0,0 +1,124 @@
+var Router = ReactRouter;
+
+var DefaultRoute = Router.DefaultRoute;
+var Link = Router.Link;
+var Route = Router.Route;
+var RouteHandler = Router.RouteHandler;
+
+var App = React.createClass({
+    render() {
+        return (
+            <div className='row'>
+                <div className='col-xs-12'>
+                    <nav>
+                        <ul className='tabs'>
+                            <li className='tab col-xs-6'><Link to='app'>Home</Link></li>
+                            <li className='tab col-xs-6'><Link to='contact'>Contact</Link></li>
+                        </ul>
+                    </nav>
+                </div>
+                <RouteHandler/>
+            </div>
+        );
+    }
+});
+
+var About = React.createClass({
+    render() {
+        return (
+            <div className='col-xs-10 col-xs-offset-1 body'>
+                <h4 className='text-center'>Hi, my name is <b>Jesse Smick</b>, and I'm a student and part-time developer.</h4>
+                <p>I mainly develop for the web, both client-side and server side.</p>
+            </div>
+        );
+    }
+});
+
+var contactMethods = [{
+    title: 'Email',
+    href: 'mailto:jesse@smick.me',
+    icon: 'envelope-o',
+    color: 'red lighten-1'
+}, {
+    title: 'Bitcoin',
+    href: '/btc',
+    icon: 'btc',
+    color: 'amber darken-1'
+}, {
+    title: 'GitHub',
+    href: '/gh',
+    icon: 'github',
+    color: 'blue lighten-1'
+}, {
+    title: 'Google+',
+    href: '/g+',
+    icon: 'google-plus',
+    color: 'red'
+}, {
+    title: 'Hacker News',
+    href: '/hn',
+    icon: 'hacker-news',
+    color: 'orange accent-4'
+}, {
+    title: 'Reddit',
+    href: '/reddit',
+    icon: 'reddit',
+    color: 'light-blue darken-2'
+}, {
+    title: 'Stack Overflow',
+    href: '/so',
+    icon: 'stack-overflow',
+    color: 'deep-orange'
+}, {
+    title: 'Twitter',
+    href: '/twitter',
+    icon: 'twitter',
+    color: 'light-blue lighten-1'
+}, {
+    title: 'Keybase',
+    href: '/keybase',
+    icon: 'key',
+    color: 'yellow darken-1'
+}, {
+    title: 'Bitbucket',
+    href: '/bitbucket',
+    icon: 'bitbucket',
+    color: 'blue darken-4'
+}];
+
+var Contact = React.createClass({
+    render() {
+        var contactTiles = [];
+
+        for (var i = 0, contactMethod; i < contactMethods.length; i++) {
+            contactMethod = contactMethods[i];
+            contactTiles.push(
+                <div className='col-xs-6 col-sm-4 col-md-3 tile' key={contactMethod.title}>
+                  <a href={contactMethod.href} title={contactMethod.title} className={'tile-content ' + contactMethod.color}>
+                    <div className='valign-wrapper tile-icon'>
+                      <i className={'fa fa-' + contactMethod.icon + ' valign'}></i>
+                    </div>
+                    <div className='tile-data'>
+                      <div className='tile-title'>{contactMethod.title}</div>
+                    </div>
+                  </a>
+                </div>
+            );
+        }
+
+        return (
+                <div className="row body grid">{contactTiles}</div>
+        );
+    }
+});
+
+var routes = (
+    <Route name='app' path='/' handler={App}>
+        <Route name='contact'handler={Contact}/>
+        <DefaultRoute handler={About}/>
+    </Route>
+);
+
+Router.run(routes, function(Handler) {
+    React.render(<Handler/>, document.getElementsByTagName('main')[0]);
+});
