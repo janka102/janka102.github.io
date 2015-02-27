2d6afa4b98f4d43530c27ef81242ae56d6052c52
Jesse Smick <janka102@gmail.com>
2015-02-27 15:09:08 -0800

    Added a header color bar

--- js/header.js
+++ js/header.js
@@ -4,7 +4,20 @@ window.addEventListener('DOMContentLoaded', function() {
             rand = prng(Math.floor(new Date() / (5 * 60 * 1000))), // change picture every 5 minutes
             background = backgrounds[rand * backgrounds.length | 0];
 
+        img.get(0).crossOrigin = 'Anonymous';
         img.attr('src', background.url);
+
+        img.on('load', function() {
+            var colorThief = new ColorThief();
+            var colors = colorThief.getPalette(img.get(0), 4);
+
+            // CSS string adding color to the .header-color-bar
+            var colorCss = '.header-color-bar{border-left-color:rgb(' + colors[0].join() + ')!important;border-right-color:rgb(' + colors[1].join() + ')!important;}';
+            colorCss += '.header-color-bar:before{background-color:rgb(' + colors[2].join() + ');}';
+            colorCss += '.header-color-bar:after{background-color:rgb(' + colors[3].join() + ');}';
+
+            $('<style />').text(colorCss).appendTo('head');
+        });
 
         img.parent().attr('data-author', background.author || 'Unknown');
     });
